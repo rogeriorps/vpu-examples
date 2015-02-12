292bd1eb07ccfbe8c89542d423b3dc4017f33c53
Rogerio Pimentel <rogerio.pimentel@freescale.com>
2015-02-12 13:31:15 -0200

    example1: Adding first example
    Signed-off-by: Rogerio Pimentel <rogerio.pimentel@freescale.com>

---
+++ mx6/example1/example1.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2004-2015 Freescale Semiconductor, Inc.
+ *
+ * Copyright (c) 2006, Chips & Media.  All rights reserved.
+ */
+
+/*
+ * The code contained herein is licensed under the GNU General Public
+ * License. You may obtain a copy of the GNU General Public License
+ * Version 2 or later at the following locations:
+ *
+ * http://www.opensource.org/licenses/gpl-license.html
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <getopt.h>
+// #include "vpu_test.h"
+
+void main(void) {
+
+	printf ("test\n");
+	return 0;
+}
